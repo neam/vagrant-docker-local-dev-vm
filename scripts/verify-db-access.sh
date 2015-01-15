883a9fb2e6cd453ed430b4ea63657a436a61e307
Fredrik Wollsén <fredrik@neam.se>
2015-01-15 14:52:31 +0200

    Not hard-coding local vm ip in verify db access

--- scripts/verify-db-access.sh
+++ scripts/verify-db-access.sh
@@ -18,7 +18,7 @@ source $script_path/variables.inc.sh
 # Verify that you can connect to the MySQL (actually MariaDB) server:
 
     export DB_PASS=$(cat "./.mariadb/pwd_$DB_APP")
-    echo "SELECT 1;" | mysql -h127.0.0.1 -P$DB_PORT -uroot -p$DB_PASS
+    echo "SELECT 1;" | mysql -h$LOCAL_VM_IP -P$DB_PORT -uroot -p$DB_PASS
 
 # Restore working directory and exit
 cd $pwd
